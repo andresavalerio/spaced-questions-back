2de3c876ec952bece0ad38e9e6ab7a6fb19f6930
Victor Jorge Carvalho Chaves <victorchaves03@gmail.com>
2023-09-18 06:29:17 -0300

    fix: notebook.schema, id column is right now

--- src/modules/notebook/schemas/notebook.schema.ts
+++ src/modules/notebook/schemas/notebook.schema.ts
@@ -5,7 +5,7 @@ export const NotebookSchema = new EntitySchema<Notebook>({
   name: "Notebook",
   target: Notebook,
   columns: {
-    id: { type: "varchar", primary: true, generated: "increment", nullable: false, unique: true },
+    id: { type: "varchar", length: 50, primary: true, nullable: false },
     name: { type: "varchar", length: 100, nullable: false },
     owner: { type: "varchar", length: 100, nullable: false },
   },
