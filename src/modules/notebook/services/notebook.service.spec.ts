f659190d41cf08d3710d6772d47c697e8aa55334
Pedro Paulo <pedro.paulo.vianna@hotmail.com>
2023-10-08 20:18:35 -0300

    refact: context for each method on notebook service tests

--- src/modules/notebook/services/notebook.service.spec.ts
+++ src/modules/notebook/services/notebook.service.spec.ts
@@ -1,63 +1,72 @@
-import { CreateNotebookDTO, INotebookService, Notebook } from "../notebook.interfaces";
+import {
+  CreateNotebookDTO,
+  INotebookService,
+  Notebook,
+} from "../notebook.interfaces";
 import { NotebookService } from "./notebook.service";
 import { NotebookRepository } from "../repositories/notebook.repository";
 
 jest.mock("../repositories/notebook.repository");
 
-const mockNotebookRepository: Notebook[] = [
-  
-];
+const mockNotebookRepository: Notebook[] = [];
 
 const mockNotebookCreation = jest
-    .spyOn(NotebookRepository.prototype, 'createNotebook')
-    .mockImplementation(async (notebookDto) => {
-        return new Promise((resolver) => {
-            setTimeout(() => {
-                const newNotebook: Notebook = {
-                    id: '1',
-                    name: notebookDto.name,
-                    owner: notebookDto.owner
-                } as Notebook
-                mockNotebookRepository.push(newNotebook)
-                resolver(newNotebook)
-            },200)
-        })
-    })
+  .spyOn(NotebookRepository.prototype, "createNotebook")
+  .mockImplementation(async (notebookDto) => {
+    return new Promise((resolver) => {
+      setTimeout(() => {
+        const newNotebook: Notebook = {
+          id: "1",
+          name: notebookDto.name,
+          owner: notebookDto.owner,
+        } as Notebook;
+        mockNotebookRepository.push(newNotebook);
+        resolver(newNotebook);
+      }, 200);
+    });
+  });
 let notebookRepository: NotebookRepository;
-let notebookService: INotebookService
-
-beforeEach(() => {
-    notebookRepository = new NotebookRepository();
-    notebookService = new NotebookService(notebookRepository);
-});
+let notebookService: INotebookService;
 
 describe("Notebook Service", () => {
-  it("Should create a empty instance of notebook service", () => {
-    expect(notebookService).toBeDefined();
+  describe("Creation of Notebook Context", () => {
+    beforeEach(() => {
+      notebookRepository = new NotebookRepository();
+      notebookService = new NotebookService(notebookRepository);
+    });
+
+    it("Should create a empty instance of notebook service", () => {
+      expect(notebookService).toBeDefined();
+    });
+
+    it("Should insert one new notebook on the repository", async () => {
+      const newNotebook: CreateNotebookDTO = {
+        name: "Testing Subjects",
+        owner: "Pedro",
+      };
+      const createdNotebook = await notebookRepository.createNotebook(
+        newNotebook
+      );
+
+      expectNewNotebookToBeReturned(newNotebook, createdNotebook);
+
+      expect(mockNotebookCreation).toHaveBeenCalled();
+    });
   });
 
-  it('Should insert one new notebook on the repository',async () => {
-
-    const newNotebook: CreateNotebookDTO = {
-        name: "Testing Subjects",
-        owner: "Pedro"
-    }
-    const createdNotebook = await notebookRepository.createNotebook(newNotebook)
-
-    expectNewNotebookToBeReturned(newNotebook, createdNotebook);
-
-    expect(mockNotebookCreation).toHaveBeenCalled()
-
-  })
-
+  describe("Getting already existent notebook context", () => {
+    it.todo("Gets a already existent notebook", () => {});
+  });
 });
 
-function expectNewNotebookToBeReturned(newNotebook: CreateNotebookDTO, createdNotebook: Notebook) {
-    expect(createdNotebook).toHaveProperty('id');
-    expect(createdNotebook.id).toBe('1');
-    expect(createdNotebook).toHaveProperty('name');
-    expect(createdNotebook.name).toBe(newNotebook.name);
-    expect(createdNotebook).toHaveProperty('owner');
-    expect(createdNotebook.owner).toBe(newNotebook.owner);
+function expectNewNotebookToBeReturned(
+  newNotebook: CreateNotebookDTO,
+  createdNotebook: Notebook
+) {
+  expect(createdNotebook).toHaveProperty("id");
+  expect(createdNotebook.id).toBe("1");
+  expect(createdNotebook).toHaveProperty("name");
+  expect(createdNotebook.name).toBe(newNotebook.name);
+  expect(createdNotebook).toHaveProperty("owner");
+  expect(createdNotebook.owner).toBe(newNotebook.owner);
 }
-
